b7b8064b79df4382625c19af0df863b78130f60b
empireofryan <rjohnson2001@gmail.com>
2016-05-18 18:02:01 -0400

    set up index, new, show, form script views

--- app/controllers/scripts_controller.rb
+++ app/controllers/scripts_controller.rb
@@ -3,4 +3,28 @@ class ScriptsController < ApplicationController
   def index
     @scripts = Script.all
   end
+
+  def new
+    @script = Script.new
+  end
+
+  def show
+    @script = Script.find(params[:id])
+  end
+
+  def create
+    @script = Script.new(script_params)
+    if @script.save
+      flash[:success] = "Your event was successfully created!"
+      redirect_to script_path(@script)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def script_params
+    params.require(:script).permit(:title, :logline)
+  end
 end
