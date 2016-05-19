a13a310cc2c50d7a86cd12d1e9f4f3d469f32403
empireofryan <rjohnson2001@gmail.com>
2016-05-19 11:12:55 -0400

    still setting up devise

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,13 @@ Rails.application.routes.draw do
     resources :scripts
   end
 
-  get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-  get 'signout', to: 'sessions#destroy', as: 'signout'
-  root 'scripts#index'
+
+
+ # get 'auth/:provider/callback', to: 'sessions#create'
+ # get 'auth/failure', to: redirect('/')
+ # get 'signout', to: 'sessions#destroy', as: 'signout'
+
+  root to: 'scripts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
