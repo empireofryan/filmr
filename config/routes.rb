20508aebc23cbb1fbff09b3576e7316dfd884a04
empireofryan <rjohnson2001@gmail.com>
2016-05-18 21:50:27 -0400

    nested script and genre resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :genres
-  resources :scripts
+  resources :genres do
+    resources :scripts
+  end
+
   root 'scripts#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
