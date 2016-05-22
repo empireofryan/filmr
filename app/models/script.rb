5758a8200f470c22bc138fd865024053563209bc
empireofryan <rjohnson2001@gmail.com>
2016-05-22 17:28:21 -0400

    sorting through model working

--- app/models/script.rb
+++ app/models/script.rb
@@ -2,4 +2,13 @@ class Script < ActiveRecord::Base
 
   belongs_to :user
   belongs_to :genre
+
+  def genre_attributes=(attributes)
+    Genre.find_or_create_by(name: name)
+  end
+
+  def self.alpha_sorted
+    Script.all.sort_by {|script| script.title}
+  end
+
 end
